14489950294fe3750101ab9947849328a81ad44e
thakur011 <aftab.yasin@live.com>
2020-07-22 00:25:46 +0400

    Second commit

--- SocialApp.API/Models/Value.cs
+++ SocialApp.API/Models/Value.cs
@@ -2,7 +2,7 @@ namespace SocialApp.API.Models
 {
     public class Value
     {
-        //This is test coment
+        //This is test coment additional
         public int Id { get; set; }
         public string Name { get; set; }
     }
