3f0a96b6fd1f1d6d90e2b90777473d82e9704252
thakur011 <aftab.yasin@live.com>
2020-07-22 00:28:39 +0400

    3rd

--- SocialApp.API/Models/Value.cs
+++ SocialApp.API/Models/Value.cs
@@ -2,7 +2,7 @@ namespace SocialApp.API.Models
 {
     public class Value
     {
-        //This is test coment additional
+        //This is test coment additionalfhdfhdfh
         public int Id { get; set; }
         public string Name { get; set; }
     }
